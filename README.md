494c078d4aba60294e79abd84335aca4f2c72556
Chris Darringer <cdarringer@yahoo.com>
2014-11-30 22:25:53 -0500

    Initial commit
    Changes to be committed:
    	new file:   README.md
    	new file:   pom.xml
    	new file:   src/main/java/com/darringer/games/sudoku/SudokuModel.java
    	new file:   src/main/java/com/darringer/games/sudoku/SudokuNakedSinglesStrategy.java
    	new file:   src/main/java/com/darringer/games/sudoku/SudokuSolver.java
    	new file:   src/main/java/com/darringer/games/sudoku/SudokuStrategy.java
    	new file:   src/main/resources/log4j.xml
    	new file:   src/test/java/com/darringer/games/sudoku/TestNakedSinglesStrategy.java
    	new file:   src/test/java/com/darringer/games/sudoku/TestSudokuSolver.java

---
+++ README.md
@@ -0,0 +1,4 @@
+# darringer-games-sudoku
+
+A Sudoku solver that uses a handful of basic strategies to solve simple to medium complexity puzzles.  
+This is a work in progress...
---
+++ src/main/java/com/darringer/games/sudoku/SudokuSolver.java
@@ -0,0 +1,58 @@
+package com.darringer.games.sudoku;
+
+
+/**
+ * Contains logic for solving a Sudoku puzzle
+ * 
+ * @author cdarringer
+ * @see SudokuModel
+ *
+ */
+public class SudokuSolver {
+
+	private SudokuStrategy nakedSinglesStrategy = new SudokuNakedSinglesStrategy();
+	
+	/**
+	 * Return the solution for a given {@link SudokuModel} or throw a 
+	 * {@link IllegalArgumentException} if the given puzzle cannot be solved.
+	 * 
+	 * @param model
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public SudokuModel solve(SudokuModel model) throws IllegalArgumentException {
+		int solvedOptionCount = (SudokuModel.SIZE * SudokuModel.SIZE);
+		int previousOptionCount = getTotalOptionCount(model);
+		while (previousOptionCount > solvedOptionCount) {	
+			// for each cell..
+			for (int x=0; x < SudokuModel.SIZE; x++) {
+				for (int y=0; y < SudokuModel.SIZE; y++) {
+					model = nakedSinglesStrategy.applyStrategy(model, x, y);
+				}
+			}
+			int currentOptionCount = getTotalOptionCount(model);
+			if (currentOptionCount == previousOptionCount) {
+				throw new IllegalArgumentException("The given model appears to be unsolvable.  Stuck at option count: " + currentOptionCount);
+			} else {
+				previousOptionCount = currentOptionCount;
+			}
+		}
+		return model;
+	}
+		
+	/**
+	 * Return the total size of the option sets in the given {@link SudokuModel} 
+	 * 
+	 * @param model
+	 * @return
+	 */
+	private int getTotalOptionCount(SudokuModel model) {
+		int optionCount = 0;
+		for (int x=0; x < SudokuModel.SIZE; x++) {
+			for (int y=0; y < SudokuModel.SIZE; y++) {
+				optionCount += model.getSet(x, y).size();
+			}
+		}
+		return optionCount;
+	}		
+}
