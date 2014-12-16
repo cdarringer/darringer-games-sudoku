135ae52b799a24290464c8f4fb503b623f158424
Chris Darringer <cdarringer@yahoo.com>
2014-12-15 21:19:28 -0500

    Ensure the naked check is not repeated unecessarily

--- src/main/java/com/darringer/games/sudoku/SudokuNakedStrategy.java
+++ src/main/java/com/darringer/games/sudoku/SudokuNakedStrategy.java
@@ -1,5 +1,6 @@
 package com.darringer.games.sudoku;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
@@ -8,6 +9,9 @@ import org.apache.log4j.Logger;
  * For each cell whose set of possible values contains a single number, 
  * remove that number from all possible column cell sets, all row cell 
  * sets, and all subsquare cell sets.
+ * <p />
+ * We remember which cells contained naked singles and were checked so 
+ * that we don't repeat the check on subsequent iterations.
  * 
  * @author cdarringer
  *
@@ -16,45 +20,53 @@ public class SudokuNakedStrategy implements SudokuStrategy {
 
 	private Logger log = Logger.getLogger(SudokuNakedStrategy.class);
 	
+	private Set<SudokuLocation> checkedLocations = new HashSet<SudokuLocation>();
+	
 	@Override
 	public SudokuModel applyStrategy(SudokuModel model, int x, int y) {
-		Set<Integer> currentSet = model.getSet(x, y);
-		if (currentSet.size() == 1) {
-			// this cell has a "naked" single value
-			// remove it from all cells in the same row...
-			log.debug(String.format("SudokuNakedSingles Strategy being applied at %d, %d...", x, y));
-			for (int i=0; i < SudokuModel.SIZE; i++) {
-				if (i != x) {
-					if (model.getSet(i, y).removeAll(currentSet)) {
-						log.debug(String.format("Removed row value at %d, %d...", i, y));						
-					}
-				}
-			}
-		
-			// remove this number as an option from all cells in the same column...
-			for (int i=0; i < SudokuModel.SIZE; i++) {
-				if (i != y) {
-					if (model.getSet(x, i).removeAll(currentSet)) {
-						log.debug(String.format("Removed column value at %d, %d...", x, i));						
-					}
-				}
-			}
-
-			// remove this number as an option from same subsquare cells...
-			int xRoot = (x / 3) * 3;
-			int yRoot = (y / 3) * 3;
-			for (int xDelta=0; xDelta <3; xDelta++) {
-				for (int yDelta=0; yDelta < 3; yDelta++) {
-					int xSubSquare = xRoot + xDelta;
-					int	ySubSquare = yRoot + yDelta;
-					if ((xSubSquare != x) || (ySubSquare != y)) {
-						if (model.getSet(xSubSquare, ySubSquare).removeAll(currentSet)) {
-							log.debug(String.format("Removed subsquare value at %d, %d...", xSubSquare, ySubSquare));													
+		if (!checkedLocations.contains(new SudokuLocation(x, y))) {
+			Set<Integer> currentSet = model.getSet(x, y);
+			if (currentSet.size() == 1) {
+				// this cell has a "naked" single value
+				// remove it from all cells in the same row...
+				log.debug(String.format("SudokuNakedSingles Strategy being applied at %d, %d...", x, y));
+				for (int i=0; i < SudokuModel.SIZE; i++) {
+					if (i != x) {
+						if (model.getSet(i, y).removeAll(currentSet)) {
+							log.info(String.format("Removed row value at %d, %d...", i, y));						
 						}
 					}
 				}
-			}
+		
+				// remove this number as an option from all cells in the same column...
+				for (int i=0; i < SudokuModel.SIZE; i++) {
+					if (i != y) {
+						if (model.getSet(x, i).removeAll(currentSet)) {
+							log.info(String.format("Removed column value at %d, %d...", x, i));						
+						}
+					}
+				}
+
+				// remove this number as an option from same subsquare cells...
+				int xRoot = (x / 3) * 3;
+				int yRoot = (y / 3) * 3;
+				for (int xDelta=0; xDelta <3; xDelta++) {
+					for (int yDelta=0; yDelta < 3; yDelta++) {
+						int xSubSquare = xRoot + xDelta;
+						int	ySubSquare = yRoot + yDelta;
+						if ((xSubSquare != x) || (ySubSquare != y)) {
+							if (model.getSet(xSubSquare, ySubSquare).removeAll(currentSet)) {
+								log.info(String.format("Removed subsquare value at %d, %d...", xSubSquare, ySubSquare));													
+							}
+						}
+					}
+				}
+				
+				// record the fact that this naked single has been checked
+				checkedLocations.add(new SudokuLocation(x, y));
+			}		
 		}
 		return model;
 	}
 }
+
